557b36f27f8f52067e813e25d654157b130f5192
Dominik Barbarić <dominik.barbaric@gmail.com>
2016-07-26 17:49:32 +0200

    Rewritten RPI code

--- jscontrol/rov.cpp
+++ jscontrol/rov.cpp
@@ -22,147 +22,143 @@ using namespace std;
 /**
 	Server receive packet structure
 */
-struct msg_t
-{
-        float f1, f2, f3, f4;
-        unsigned mask;
+struct __attribute__((__packed__)) srv_msg_t {
+    float f1, f2, f3, f4;
+    unsigned mask;
 };
+
+/**
+	Arduino send packet structure
+*/
+struct __attribute__((__packed__)) ard_msg_t {
+	unsigned int header;
+	unsigned char thr_left, thr_right, thr_center;
+	unsigned char mask;
+}
 
 enum {left_motor, right_motor, center_motor};
 
 int sock, port, ready = 0;
-msg_t msg;
+srv_msg_t msg;
+ard_msg_t ard_msg;
 
-void socket_setup()
-{
-        int socket_desc;
-        socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-        sockaddr_in sock_name;
-        sock_name.sin_family = AF_INET;
-        sock_name.sin_addr.s_addr = htonl(INADDR_ANY);
-        sock_name.sin_port = htons(1234);
-		
-        bind(socket_desc, (sockaddr*)& sock_name, sizeof(sock_name));
-        listen(socket_desc, 1);
-        fprintf(stderr, "Listening on port %d\n", port);
-		
-        sock = accept(socket_desc, 0, 0);
-        close(socket_desc);
+void socket_setup() {
+
+	int socket_desc;
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	sockaddr_in sock_name;
+	sock_name.sin_family = AF_INET;
+	sock_name.sin_addr.s_addr = htonl(INADDR_ANY);
+	sock_name.sin_port = htons(1234);
+	
+	bind(socket_desc, (sockaddr*)& sock_name, sizeof(sock_name));
+	listen(socket_desc, 1);
+	fprintf(stderr, "Listening on port %d\n", port);
+	
+	sock = accept(socket_desc, 0, 0);
+	close(socket_desc);
 }
 
-void motor_intensity(int id, float intensity)
-{
-        intensity*=50;
-        if(intensity>=0)
-        {
-                if(id==left_motor)
-                {
-			msg.mask &= ~(1 << 2);
-			msg.f1 = intensity;
-                }
-                else if(id==right_motor)
-                {
-			msg.mask &= ~(1 << 3);
-			msg.f2 = intensity;
-                }
-                else
-                {
-			msg.mask &= ~(1 << 1);
-			msg.f3 = intensity;
-                }
-        }
-        else
-        {
-                if(id==left_motor)
-                {
-			msg.mask |= 1 << 2;
-			msg.f1 = -intensity;
-                }
-                else if(id==right_motor)
-                {
-			msg.mask |= 1 << 3;
-			msg.f2 = -intensity;
-                }
-                else
-                {
-			msg.mask |= 1 << 1;
-			msg.f3 = -intensity;
+void motor_intensity(int id, float throttle_float) {
+	
+	unsigned char throttle;
+	throttle = (unsigned char) abs((255 * throttle_float));
+	
+	if (throttle_float >= 0) {
+		if (id == left_motor) {
+			ard_msg.mask &= ~(1 << 2);
+			ard_msg.thr_left = throttle;
 		}
-        }
+		else if (id == right_motor) {
+			ard_msg.mask &= ~(1 << 3);
+			ard_msg.thr_right = throttle;
+		}
+		else if (id == center_motor) {
+			ard_msg.mask &= ~(1 << 1);
+			ard_msg.thr_center = throttle;
+		}
+	} else {
+		if (id == left_motor) {
+			ard_msg.mask |= 1 << 2;
+			ard_msg.thr_left = -throttle;
+		}
+		else if (id == right_motor) {
+			ard_msg.mask |= 1 << 3;
+			ard_msg.thr_right = -throttle;
+		} else if (id == center_motor) {
+			ard_msg.mask |= 1 << 1;
+			ard_msg.thr_center = -throttle;
+		}
+	}
 }
 
-void handle_input(float axis0, float axis1, float axis2, float axis3, unsigned mask)
-{
-	msg.f1 = msg.f1 < 0 ? -1 : 0;
-	msg.f2 = msg.f2 < 0 ? -1 : 0;
+void handle_input(float axis0, float axis1, float axis2, float axis3, unsigned mask) {
 
-        if(mask&1u<<8) //select dugme - naredba quit
-                exit(0);
-        float angle = atan2(-axis1, axis0);
-        float r = std::hypot(axis0, -axis1); if(r>1) r=1;
-        //fprintf(stderr, "angle: %.2f, r: %.2f\n, r2: %.2f\n", angle, r, fabs(axis2));
+	if (mask&1u<<8) //select dugme - naredba quit
+			exit(0);
+			
+	// The angle and the radius calculated below adhere to the
+	// left analog stick as represented by the image below
+	//      --- PI/2 ---
+	//     /            \ r=1
+	//    /              \
+	// +-PI      o r=0    0
+	//    \              /
+	//     \            /
+	//      -- -PI/2 ---
+	float angle = atan2(-axis1, axis0);
+	float r = std::hypot(axis0, -axis1); if(r>1) r=1;
+	//fprintf(stderr, "angle: %.2f, r: %.2f, r2: %.2f\n", angle, r, fabs(axis2));
 
-        if(fabs(axis2)<0.05)
-                motor_intensity(center_motor, 0);
-        else
-                motor_intensity(center_motor, axis2);
-
-        if(mask & (1u<<6))
-        {
-                motor_intensity(left_motor, -1);
-                motor_intensity(right_motor, 1);
-        }
-        else if(mask& (1u<<7))
-        {
-                motor_intensity(left_motor, 1);
-                motor_intensity(right_motor, -1);
-        }
-        else if(mask& (1u<<3))
-        {
-                msg.mask |= 1 << 7;
-        }
-
-
-        if(mask&(3u<<6))
-        {
-                //bilo je pritisnuto okretanje, necemo da gledamo analognu kapicu
-                return;
-        }
-
-
-        if(r<0.05)
-        {
-                motor_intensity(left_motor, +0);
-                motor_intensity(right_motor, +0);
-        }
-        else
-        {
-                if(0 <= angle && angle < M_PI_2)
-                {
-                        motor_intensity(left_motor, r);
-                        motor_intensity(right_motor, r*(1-axis0));
-                }
-                else if((M_PI_2 <= angle && angle < M_PI) || angle == ((float) -M_PI))
-                {
-                        motor_intensity(right_motor, r);
-                        motor_intensity(left_motor, r*(1+axis0));
-                }
-                else if(-7 * M_PI_8 <= angle && angle < -5 * M_PI_8)
-                {
-                        motor_intensity(right_motor, -r);
-                        motor_intensity(left_motor, 0);
-                }
-                else if(-3 * M_PI_8 <= angle && angle < -M_PI_8)
-                {
-                        motor_intensity(left_motor, -r);
-                        motor_intensity(right_motor, 0);
-                }
-                else if(-5 * M_PI_8 <= angle && angle <-3*M_PI_8)
-                {
-                        motor_intensity(left_motor, -r );
-                        motor_intensity(right_motor, -r );
-                }
+	// Nepoznata svrha
+	// Promijenjena maska za ledice
+	if (mask& (1u<<3)) {
+		msg.mask |= 1 << 7;
 	}
+
+	
+	// LEFT AND RIGHT MOTOR
+	// Promjena smjerova releja ne bi se smjela dogadjati unutar bilo koje mrtve zone
+	//
+	// Interval r < 0.05 is taken as a dead zone
+	if (r<0.05) {
+		motor_intensity(left_motor, +0);
+		motor_intensity(right_motor, +0);
+	} else {
+		// MOVING FORWARD
+		if (0 <= angle && angle < M_PI_2) {
+			// axis0 > 0
+			motor_intensity(left_motor, r*axis0);
+			motor_intensity(right_motor, r*(1-axis0));
+		}
+		else if ((M_PI_2 <= angle && angle < M_PI) || angle == ((float) -M_PI)) {
+			// axis0 < 0
+			motor_intensity(right_motor, r*(-axis0));
+			motor_intensity(left_motor, r*(1+axis0));
+		}
+		// MOVING BACKWARD
+		// Intervals <-PI_8, 0> and <-PI, -7/8 PI> are taken as dead zones.
+		// Therefore, the axis0 is normalized to cos(M_PI_8) so as to preserve the throttle range.
+		else if (-M_PI_2 <= angle && angle < -M_PI_8) {
+			axis0 /= cos(M_PI_8);
+			// axis0 > 0
+			motor_intensity(left_motor, -r*axis0);
+			motor_intensity(right_motor, -r*(1-axis0);
+		}
+		else if (-7*M_PI_8 < angle && angle < -M_PI_2) {
+			axis0 /= cos(M_PI_8);
+			// axis0 < 0
+			motor_intensity(right_motor, -r*(-axis0));
+			motor_intensity(left_motor, r*(1+axis0));
+		}
+	}
+	
+	// CENTER MOTOR
+	if(fabs(axis2)<0.05)
+			motor_intensity(center_motor, 0);
+	else
+			motor_intensity(center_motor, axis2);
+	
 }
 
 char buf[1024];
@@ -187,56 +183,57 @@ int main (int argc, char** argv)
 	
 	// We use the first responsive ttyUSB device
 	int fd;	
-	for (int i = 0; i < 10; ++i)
-	{
+	for (int i = 0; i < 10; ++i) {
 		path[11] = ((char)(i + '0'));
+		
 		fd = serialOpen(path, 9600);
-		cout << path << ' ' << fd << endl;
-		if (fd != -1) break;
+		
+		if (fd != -1){
+			fprintf(stderr, "%s", path);
+			break;
+		}
 	}
-	if (fd == -1) return 1;
+	if (fd == -1)
+		return 1;
 
-	while (1) 
-	{
+	while (1) {
 		// Receiving packet
 		int len = recv(sock, buf, 4*4+4, MSG_WAITALL);
                 msg = *(msg_t*)(void*)(&buf);
+		//fprintf(stderr, "%1.2f %1.2f %1.2f 0x%2x\n", msg.f1, msg.f2, msg.f3, msg.mask);
 		
 		// Mask has the low bit 1
 		// This is an attempt to determine the position of the mask in the struct received on Arduino
 		msg.mask |= 1;
 		
 		if (mask == 255)
-		{
 			mask = msg.mask;
-		}
 
 		char spi_data[5];
 		unsigned in;		
-
+		
+		ard_msg.header = 0xFF00FF00;
+		ard_msg.mask = 0x80;
+		
 		handle_input(msg.f1, msg.f2, msg.f3, msg.f4, msg.mask);
 		
 		digitalWrite(24, !digitalRead(24));
 		
 		//spi_data[0] = (char) 255;
-		if (msg.f1 < 0)
-		{
+		if (msg.f1 < 0) {
 			//spi_data[2] = ((char) 0);
             msg.mask |= (mask & 1 << 3);
 	    }
-		else
-        {
+		else {
 			//spi_data[2] = ((char) ((int) (msg.f1 <= 1 ? msg.f1 * 100 : msg.f1)));
             mask |= (msg.mask & 1 << 3);                
 	    }
 	    
-		if (msg.f2 < 0)
-        {
+		if (msg.f2 < 0) {
 			//spi_data[3] = ((char) 0);
             msg.mask |= (mask & 1 << 1);
         }
-        else
-        {
+        else {
 			//spi_data[3] = ((char) ((int) (msg.f2 <= 1 ? msg.f2 * 100 : msg.f2)));
             mask |= (msg.mask & 1 << 1);
         }
@@ -251,13 +248,19 @@ int main (int argc, char** argv)
 		//spi_data[3] &= ~(1 << 7);
 
 		//spi_out << spi_data[0] << flush;
-		for (int i = 1; i < 4; ++i)
-		{
-			spi_data[i] &= ~(1);
-			serialPutchar(fd, spi_data[i]);
-		}
+		//for (int i = 1; i < 4; ++i) {
+		//	spi_data[i] &= ~(1);
+		//	serialPutchar(fd, spi_data[i]);
+		//}
 		
-		serialPutchar(fd, spi_data[4]);
+		//serialPutchar(fd, spi_data[4]);
+		
+		for (int i = 0; i < 4; i++)
+			serialPutchar(fd, (char) ((ard_msg.header >> 8*(4-i)) & 0xFF);
+		serialPutchar(fd, ard_msg.thr_left);
+		serialPutchar(fd, ard_msg.thr_right);
+		serialPutchar(fd, ard_msg.thr_center);
+		serialPutchar(fd, ard_msg.mask);
 		
 	}	
 	
